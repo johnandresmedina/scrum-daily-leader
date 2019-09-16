f574f161aaeb52d3b7a0514d049eba092aaaab8a
John Medina <jhonandresmedina@gmail.com>
2019-09-16 15:51:54 -0500

    Updates

---
+++ precache-manifest.781191b29fbc83b2a3370ff9ae7ec2c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901dd23b973de1af890512851c311e0c",
+    "url": "/scrum-daily-leader/index.html"
+  },
+  {
+    "revision": "7d59ec88f21d4200ede8",
+    "url": "/scrum-daily-leader/static/css/2.9c7ff97b.chunk.css"
+  },
+  {
+    "revision": "b42a2a874b1b62cafaef",
+    "url": "/scrum-daily-leader/static/css/main.59a6a6a5.chunk.css"
+  },
+  {
+    "revision": "7d59ec88f21d4200ede8",
+    "url": "/scrum-daily-leader/static/js/2.925cfa0e.chunk.js"
+  },
+  {
+    "revision": "b42a2a874b1b62cafaef",
+    "url": "/scrum-daily-leader/static/js/main.6d33edc1.chunk.js"
+  },
+  {
+    "revision": "0a9cdb6aad6a85eb424a",
+    "url": "/scrum-daily-leader/static/js/runtime~main.1590ce65.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scrum-daily-leader/static/media/logo.5d5d9eef.svg"
+  }
+]);
