eb56b7ae699d5b65db4f527a88087a6cee9378ce
John Medina <jhonandresmedina@gmail.com>
2019-09-28 12:06:57 -0500

    splitting participants into container and presentational components

--- src/participants/containers/participants.js
+++ src/participants/containers/participants.js
@@ -1,56 +1,15 @@
-import './participants.scss';
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import isEqual from 'lodash/isEqual';
-import get from 'lodash/get';
-import { FormattedMessage } from 'react-intl';
 
 import { removeParticipant } from '../actions';
+import Participants from '../components/participants';
 
-import Participant from '../components/participant';
-
-const Participants = ({ participants, selectedParticipant, removeParticipant }) => {
-    const getParticipantsCard = () => {
-        const currentName = selectedParticipant ? selectedParticipant.name : null;
-
-        return participants.map((participant, index) => {
-            const name = participant.name;
-            const active = isEqual(currentName, name);
-
-            return (
-                <div key={`participant-${index}`} className='col-md-3'>
-                    <Participant
-                        active={active}
-                        description={get(participant, 'description', 'An awesome member!')}
-                        index={index}
-                        name={name}
-                        removeParticipant={removeParticipant}
-                    />
-                </div>
-            );
-        });
-    };
-
-    const buildContent = () => {
-        return participants.length ? (
-            getParticipantsCard()
-        ) : (
-            <div className='participants__no-content'>
-                <FormattedMessage id='participants-empty' />
-            </div>
-        );
-    };
-
-    return (
-        <div className='participants'>
-            <div className='row'>{buildContent()}</div>
-        </div>
-    );
+const ParticipantsContainer = ({ participants, selectedParticipant, removeParticipant }) => {
+    return <Participants {...{ participants, selectedParticipant, removeParticipant }} />;
 };
 
-Participants.propTypes = {
+ParticipantsContainer.propTypes = {
     participants: PropTypes.array.isRequired,
     selectedParticipant: PropTypes.object,
     removeParticipant: PropTypes.func.isRequired,
@@ -62,4 +21,4 @@ export default connect(
         selectedParticipant: state.participantsState.selectedParticipant,
     }),
     { removeParticipant },
-)(Participants);
+)(ParticipantsContainer);
